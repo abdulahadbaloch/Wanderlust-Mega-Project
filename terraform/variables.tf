1ed49a16282b57644a314b21ca58c5ff82e403c8
abdulahadbaloch <abdul12101ahad@gmail.com>
2026-08-28 15:54:14 +0500

    Change default AWS region and add Ubuntu AMI data source
    Updated the AWS region default value and added data source for the latest Ubuntu AMI.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,14 +1,31 @@
 variable "aws_region" {
   description = "AWS region where resources will be provisioned"
-  default     = "us-east-2"
-}
-
-variable "ami_id" {
-  description = "AMI ID for the EC2 instance"
-  default     = "ami-085f9c64a9b75eed5"
+  default     = "us-east-1"
 }
 
 variable "instance_type" {
   description = "Instance type for the EC2 instance"
   default     = "t2.large"
+}
+
+
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
 }
