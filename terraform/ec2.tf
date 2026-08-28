4fbb1b786adf505dea8bba40762588de5c17fd1e
abdulahadbaloch <abdul12101ahad@gmail.com>
2026-08-28 16:54:02 +0500

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -10,6 +10,7 @@ resource "aws_security_group" "allow_user_to_connect" {
   name        = "allow TLS"
   description = "Allow user to connect"
   vpc_id      = aws_default_vpc.default.id
+
   ingress {
     description = "port 22 allow"
     from_port   = 22
@@ -17,13 +18,15 @@ resource "aws_security_group" "allow_user_to_connect" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   egress {
-    description = " allow all outgoing traffic "
+    description = "allow all outgoing traffic"
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   ingress {
     description = "port 80 allow"
     from_port   = 80
@@ -31,6 +34,7 @@ resource "aws_security_group" "allow_user_to_connect" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   ingress {
     description = "port 443 allow"
     from_port   = 443
@@ -38,6 +42,7 @@ resource "aws_security_group" "allow_user_to_connect" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   tags = {
     Name = "mysecurity"
   }
@@ -63,9 +68,11 @@ resource "aws_instance" "testinstance" {
   instance_type   = var.instance_type
   key_name        = aws_key_pair.deployer.key_name
   security_groups = [aws_security_group.allow_user_to_connect.name]
+
   tags = {
     Name = "Automate"
   }
+
   root_block_device {
     volume_size = 30
     volume_type = "gp3"
