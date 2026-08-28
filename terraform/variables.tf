bb42939292ffe3743adb0837ac4ebe8762ece76b
abdulahadbaloch <abdul12101ahad@gmail.com>
2026-08-28 16:53:39 +0500

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,25 +7,3 @@ variable "instance_type" {
   description = "Instance type for the EC2 instance"
   default     = "t2.large"
 }
-
-
-
-data "aws_ami" "ubuntu" {
-  most_recent = true
-  owners      = ["099720109477"] # Canonical
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
-
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
-}
