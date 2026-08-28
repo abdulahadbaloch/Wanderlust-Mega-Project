89d924cd4ede6a8f313d6941e2aa189f43cd16f8
abdulahadbaloch <abdul12101ahad@gmail.com>
2026-08-28 16:29:30 +0500

    Update AWS provider version and backend settings
    Updated AWS provider version constraint and backend configuration.

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -2,8 +2,15 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "6.62.0"
+      version = "~> 6.0"
     }
+  }
+
+  backend "s3" {
+    bucket  = "your-name-tf-state-2026"   # ← updated to match your actual bucket
+    key     = "env/dev/terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
   }
 }
 
